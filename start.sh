25e43b8fe5aae1d72ddddb10332ecd72180addc5
Kushcabbage <alhockly@gmail.com>
2019-08-09 22:18:51 +0100

    added display fix + clear cache to ensure code changes are reflected

--- start.sh
+++ start.sh
@@ -1,6 +1,8 @@
+export DISPLAY=:0.0
 killall chromium-browser
 sudo killall python3
 
+rm -rf ~/.cache/chromium
 sudo python3 FRIDAY-Mirror.py &
 sleep 10
 chromium-browser http://localhost:8000/main.html --kiosk &
