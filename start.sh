0df8c29e85aab6b6f34ea3098601506fd87f2659
Kushcabbage <alhockly@gmail.com>
2019-08-10 12:48:12 +0100

    Added a clock and disown to start script

--- start.sh
+++ start.sh
@@ -3,6 +3,6 @@ killall chromium-browser
 sudo killall python3
 
 rm -rf ~/.cache/chromium
-sudo python3 FRIDAY-Mirror.py &
+sudo python3 FRIDAY-Mirror.py & disown
 sleep 10
-chromium-browser http://localhost:8000/main.html --kiosk &
+chromium-browser http://localhost:8000/main.html --kiosk & disown
