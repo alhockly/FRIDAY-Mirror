12cc46803164e1957b9f042e1e3828dfb51b1db6
Kushcabbage <alhockly@gmail.com>
2019-08-21 01:14:24 +0100

    finally the scripts work

--- start.sh
+++ start.sh
@@ -5,4 +5,6 @@ sudo killall python3
 sudo rm -rf ~/.cache/chromium
 sudo nohup python3 FRIDAY-Mirror.py &
 sleep 10
+chromium-browser --kiosk --incognito http://localhost:8000/main.html &
+sleep 5
 ./keepchromiumopen.sh &
